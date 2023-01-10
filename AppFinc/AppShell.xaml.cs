b707b4e9d9509074227d5af68fec3c223f2b6031
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-01-10 19:54:19 -0300

    Add project files.

---
+++ AppFinc/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppFinc;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppFinc/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppFinc;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
