8590098608c838789c0e9442f138270779ccf9df
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-01-11 19:12:11 -0300

    create model

---
+++ AppFinc/Models/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFinc.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public TransactionType Type { get; set; }
+
+        public String Name { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+
+        public decimal Value { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Income,
+        Expenses
+    }
+}
