f11283a26eb1cf7d3c54dfc1c293be90bcd41304
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-01-11 21:28:54 -0300

    create TransactionRepository

---
+++ AppFinc/Repositories/ITransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using AppFinc.Models;
+
+namespace AppFinc.Repositories
+{
+    public interface ITransactionRepository
+    {
+        void Add(Transaction transaction);
+        void Delete(Transaction transaction);
+        List<Transaction> GetAll();
+        void Update(Transaction transaction);
+    }
+}
---
+++ AppFinc/Repositories/TransactionRepository.cs
@@ -0,0 +1,25 @@
+﻿using AppFinc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFinc.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        public TransactionRepository()
+        {
+
+        }
+
+        public List<Transaction> GetAll() { }
+
+        public void Add(Transaction transaction) { }
+
+        public void Update(Transaction transaction) { }
+
+        public void Delete(Transaction transaction) { }
+    }
+}
