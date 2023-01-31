3eb8dcb8042f80a479f1bde3606d75a6d79bf5b7
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-01-30 21:11:45 -0300

    add views

---
+++ AppFinc/Views/TransactionAdd.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppFinc.Views;
+
+public partial class TransactionAdd : ContentPage
+{
+	public TransactionAdd()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppFinc/Views/TransactionEdit.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppFinc.Views;
+
+public partial class TransactionEdit : ContentPage
+{
+	public TransactionEdit()
+	{
+		InitializeComponent();
+	}
+}
